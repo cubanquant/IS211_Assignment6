79530dea91f35931a536b17e7eba9689c31d7a1b
cuban.quant <cuban.quant@gmail.com>
2020-10-25 22:53:51 -0400

    Create conversions.py

---
+++ conversions.py
@@ -0,0 +1,13 @@
+
+def convertCelsiusToKelvin(celsius):
+    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"
+    kelvins = 0
+    
+    return kelvins
+
+
+def convertCelsiusToFahrenheit(celsius):
+    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
+    fahrenheit = 0
+    
+    return fahrenheit
